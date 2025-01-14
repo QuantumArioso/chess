f8b552d4a21a7c7093afaa7eb2b6a0af8908379d
QuantumArioso <anwyntanner@gmail.com>
2025-01-14 13:44:51 -0700

    create pieces and fill in board

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,6 +7,39 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    private ChessPiece[][] squares = new ChessPiece[8][8];
+    private ChessPiece bKing = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING);
+    private ChessPiece bQueen = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN);
+    private ChessPiece bBishopA = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
+    private ChessPiece bBishopB = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
+    private ChessPiece bKnightA = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
+    private ChessPiece bKnightB = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
+    private ChessPiece bRookA = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
+    private ChessPiece bRookB = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
+    private ChessPiece bPawnA = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+    private ChessPiece bPawnB = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+    private ChessPiece bPawnC = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+    private ChessPiece bPawnD = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+    private ChessPiece bPawnE = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+    private ChessPiece bPawnF = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+    private ChessPiece bPawnG = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+    private ChessPiece bPawnH = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+    private ChessPiece wKing = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
+    private ChessPiece wQueen = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN);
+    private ChessPiece wBishopA = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+    private ChessPiece wBishopB = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+    private ChessPiece wKnightA = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+    private ChessPiece wKnightB = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+    private ChessPiece wRookA = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+    private ChessPiece wRookB = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+    private ChessPiece wPawnA = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+    private ChessPiece wPawnB = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+    private ChessPiece wPawnC = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+    private ChessPiece wPawnD = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+    private ChessPiece wPawnE = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+    private ChessPiece wPawnF = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+    private ChessPiece wPawnG = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+    private ChessPiece wPawnH = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
 
     public ChessBoard() {
         
@@ -19,7 +52,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        squares[position.getRow()][position.getColumn()] = piece;
     }
 
     /**
@@ -30,7 +63,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return squares[position.getRow()][position.getColumn()];
     }
 
     /**
@@ -38,6 +71,38 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        // puts pieces where they belong when game starts
+        squares[0][0] = bRookA;
+        squares[0][1] = bKnightA;
+        squares[0][2] = bBishopA;
+        squares[0][3] = bQueen;
+        squares[0][4] = bKing;
+        squares[0][5] = bBishopB;
+        squares[0][6] = bKnightB;
+        squares[0][7] = bRookB;
+        squares[1][0] = bPawnA;
+        squares[1][1] = bPawnB;
+        squares[1][2] = bPawnC;
+        squares[1][3] = bPawnD;
+        squares[1][4] = bPawnE;
+        squares[1][5] = bPawnF;
+        squares[1][6] = bPawnG;
+        squares[1][7] = bPawnH;
+        squares[6][0] = wPawnA;
+        squares[6][1] = wPawnB;
+        squares[6][2] = wPawnC;
+        squares[6][3] = wPawnD;
+        squares[6][4] = wPawnE;
+        squares[6][5] = wPawnF;
+        squares[6][6] = wPawnG;
+        squares[6][7] = wPawnH;
+        squares[7][0] = wRookA;
+        squares[7][1] = wKnightA;
+        squares[7][2] = wBishopA;
+        squares[7][3] = wQueen;
+        squares[7][4] = wKing;
+        squares[7][5] = wBishopB;
+        squares[7][6] = wKnightB;
+        squares[7][7] = wRookB;
     }
 }
