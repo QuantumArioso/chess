2bdc8405b0eb109cc884ce7762d8846cadbb1f7a
QuantumArioso <anwyntanner@gmail.com>
2025-01-20 22:08:39 -0700

    deleting duplicate code

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,8 +1,38 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public interface PieceMovesCalculator {
     // see this diagram: https://github.com/softwareconstruction240/softwareconstruction/blob/main/chess/0-chess-moves/ChessPhase0Design.png
     Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position);
+
+    /**
+     * Used for rook, bishop, and queen
+     * @param board chess board located on
+     * @param y current position row
+     * @param x current position column
+     * @param xDirection -1 for left, +1 for right, 0 for neither
+     * @param yDirection -1 for south, +1 for north, 0 for neither
+     * @return ArrayList of possible positions
+     */
+    default Collection<ChessPosition> findMoves(ChessBoard board, ChessPosition oldPosition, int y, int x, int xDirection, int yDirection) {
+        y += yDirection;
+        x += xDirection;
+        ChessPosition newPosition = new ChessPosition(y, x);
+        ArrayList<ChessPosition> newPositions = new ArrayList<>();
+
+        while (board.inBounds(newPosition) && board.isEmpty(newPosition)) {
+            newPositions.add(newPosition);
+            y += yDirection;
+            x += xDirection;
+            newPosition = new ChessPosition(y, x);
+        }
+        // check if can capture
+        if (board.inBounds(newPosition) && board.isNotOccupiedBySelf(oldPosition, newPosition)) {
+            newPositions.add(newPosition);
+        }
+
+        return newPositions;
+    }
 }
--- shared/src/main/java/chess/QueenMovesCalculator.java
+++ shared/src/main/java/chess/QueenMovesCalculator.java
@@ -28,33 +28,4 @@ public class QueenMovesCalculator implements PieceMovesCalculator {
 
         return possibleMoves;
     }
-
-    /**
-     *
-     * @param board chess board located on
-     * @param y current position row
-     * @param x current position column
-     * @param xDirection -1 for left, +1 for right, 0 for neither
-     * @param yDirection -1 for south, +1 for north, 0 for neither
-     * @return ArrayList of possible positions
-     */
-    public Collection<ChessPosition> findMoves(ChessBoard board, ChessPosition oldPosition, int y, int x, int xDirection, int yDirection) {
-        y += yDirection;
-        x += xDirection;
-        ChessPosition newPosition = new ChessPosition(y, x);
-        ArrayList<ChessPosition> newPositions = new ArrayList<>();
-
-        while (board.inBounds(newPosition) && board.isEmpty(newPosition)) {
-            newPositions.add(newPosition);
-            y += yDirection;
-            x += xDirection;
-            newPosition = new ChessPosition(y, x);
-        }
-        // check if can capture
-        if (board.inBounds(newPosition) && board.isNotOccupiedBySelf(oldPosition, newPosition)) {
-            newPositions.add(newPosition);
-        }
-
-        return newPositions;
-    }
 }
