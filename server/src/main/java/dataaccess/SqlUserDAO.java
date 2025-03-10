c33ce2652a7c49b4667050f75748fc4310d2b738
QuantumArioso <anwyntanner@gmail.com>
2025-03-10 12:44:24 -0600

    setting up SqlUserDAO

--- server/src/main/java/dataaccess/SqlUserDAO.java
+++ server/src/main/java/dataaccess/SqlUserDAO.java
@@ -1,6 +1,10 @@
 package dataaccess;
 
 import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class SqlUserDAO extends SqlDAO implements UserDAO {
     @Override
@@ -9,12 +13,32 @@ public class SqlUserDAO extends SqlDAO implements UserDAO {
     }
 
     @Override
-    public void createUser(UserData userData) {
-        throw new RuntimeException("Not implemented");
+    public void createUser(UserData userData) throws SQLException {
+        String username = userData.username();
+        var conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/pet_store", "root", "monkeypie");
+
+        if (username.matches("[a-zA-Z]+")) {
+            var statement = "INSERT INTO pet (name) VALUES(?)";
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.setString(1, username);
+                preparedStatement.executeUpdate();
+            }
+        }
     }
 
     @Override
     public void deleteAllUserData() {
         throw new RuntimeException("Not implemented");
+    }
+
+    private String hashUserPassword(String clearTextPassword) {
+        return BCrypt.hashpw(clearTextPassword, BCrypt.gensalt());
+    }
+
+    private boolean verifyUser(String username, String providedClearTextPassword) {
+        // read the previously hashed password from the database
+        var hashedPassword = readHashedPasswordFromDatabase(username);
+
+        return BCrypt.checkpw(providedClearTextPassword, hashedPassword);
     }
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -2,10 +2,12 @@ package dataaccess;
 
 import model.UserData;
 
+import java.sql.SQLException;
+
 public interface UserDAO {
     UserData getUser(String username);
 
-    void createUser(UserData userData);
+    void createUser(UserData userData) throws SQLException;
 
     void deleteAllUserData();
 }
