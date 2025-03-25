8d552b411089ba1b5071f8993445239afdcb9d94
QuantumArioso <anwyntanner@gmail.com>
2025-03-24 19:04:43 -0600

    register tests work

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,12 +1,12 @@
 package client;
 
+import exceptions.UnavailableException;
 import org.junit.jupiter.api.*;
 import server.Server;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
@@ -28,16 +28,22 @@ public class ServerFacadeTests {
     }
 
     @Test
+    @DisplayName("Register User Success")
+    public void createUserPositive() throws IOException {
+        assertEquals(36, ServerFacade.register("amity", "abomination8", "mittens@gmail.com").length());
+    }
+
+    @Test
+    @DisplayName("Register User Already Exists")
+    public void createUserNegative() throws IOException {
+        assertThrows(UnavailableException.class, () -> ServerFacade.register("raine", "888", "rainestorm@gmail.com"));
+    }
+
+    @Test
     @DisplayName("Clear All Data")
     public void clearTest() throws IOException {
         ServerFacade.clear();
         // if this works, I can successfully add the exact same user as before
         assertEquals(36, ServerFacade.register("raine", "888", "rainestorm@gmail.com").length());
-    }
-
-    @Test
-    @DisplayName("Create User Success")
-    public void createUserPositive() {
-
     }
 }
